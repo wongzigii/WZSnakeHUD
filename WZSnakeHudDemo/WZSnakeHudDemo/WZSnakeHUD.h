c7d600c5c87ad6d22e5334ac350cc7854f1c800c
Wongzigii <wongzigii@outlook.com>
2014-12-06 17:50:23 +0800

    [Fix]

--- WZSnakeHudDemo/WZSnakeHudDemo/WZSnakeHUD.h
+++ WZSnakeHudDemo/WZSnakeHudDemo/WZSnakeHUD.h
@@ -15,7 +15,7 @@ static const CGFloat WZSnakeHUDFrameHeight   = 76.0f;
 static const CGFloat WZSnakeLengthIteration  = 8.0f;
 static const CGFloat WZSnakeFramePerSecond   = 60.0f;
 
-/**
+/*!
  * define the line direction
  */
 typedef enum {
